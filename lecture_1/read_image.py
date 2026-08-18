d9d96f9f26bf76253fe0aa0f06cfbab05477ca30
Rohit <rhtshere@gmail.com>
2026-08-18 22:43:38 +0530

    read img

---
+++ lecture_1/read_image.py
@@ -0,0 +1,12 @@
+import cv2
+
+img1 = cv2.imread('img1.jpg')
+print(img1.shape)
+
+
+img1 = cv2.resize(img1 , (1024, 1365))
+
+cv2.imwrite("new_img.jpg", img1)
+
+cv2.imshow('Image 1', img1)
+cv2.waitKey(0)
